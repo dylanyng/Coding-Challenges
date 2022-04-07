3075ca252989f5fef966c80ed541ddb9db5a9abd
dylanyng <dylanyoung@hey.com>
2022-04-07 13:27:41 -0400

    Add coding challenge for 04-07-22

---
+++ CC_2022-04-07/app.js
@@ -0,0 +1,15 @@
+// Name: Reversed sequence
+
+// Directions:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+const reverseSeq = n => {
+  let arr = [];
+  for (let i = 1; i <= n; n--) {
+    arr.push(n);
+  }
+  return arr;
+};

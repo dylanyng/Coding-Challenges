f58230e8ee3f375366abb9d4f28bb663f45c2ee3
dylanyng <dylanyoung@hey.com>
2022-05-01 14:29:16 -0400

    Add coding challenge for 05-01-22

---
+++ CC_2022-05-01/app.js
@@ -0,0 +1,28 @@
+// Name: Count by X
+
+
+// Directions: 
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+
+function countBy(x, n) {
+  let z = [];
+  let y = x;
+  for (let i = 0; i < n; i++) {
+    z.push(y);
+    y += x;
+  }
+  return z;
+}
+// Create y variable that equals the initial x value
+// Create for loop that runs the length of n
+// Each time the loop runs, push the y value to the array then add x to the y variable

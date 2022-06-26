ed90f5bf174ab77b896f8a1b4ccad30ae693b09f
Dylan Young <dylanyoung@Dylans-MacBook-Pro.local>
2022-06-26 11:54:41 -0400

    Add coding challenge for 06-26-22

---
+++ CC_2022-06-26/app.js
@@ -0,0 +1,22 @@
+// Name: Multiples of 3 or 5
+
+
+// Directions: 
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// My solution:
+// Create a variable with the value of 0
+// Create a for loop, for the duration of the number argument, starting with 1
+// If i % 5 === 0 OR i % 3 === 0, then += it to the variable
+// Return the variable
+function solution(number){
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 5 === 0 || i % 3 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}

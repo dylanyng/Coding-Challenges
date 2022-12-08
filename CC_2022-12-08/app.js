6f58f6a6dc2add295c3e083126f0cb6378edd011
dylanyng <dylanyoung@hey.com>
2022-12-08 14:55:36 -0500

    Add coding challenge for 12-08-22

---
+++ CC_2022-12-08/app.js
@@ -0,0 +1,10 @@
+// Name: Reversed Strings
+
+
+// Instructions: 
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+const solution = str => str.split('').reverse().join('');
